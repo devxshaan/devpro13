ae64cc0580382076dcd7ce841db6527e37778452
devxshaan <sukhrajsingh7006@gmail.com>
2026-06-17 14:29:29 +0530

    SEO Generator

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Bolt App' }}</title>
-
+    {!! SEO::generate() !!}
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700" rel="stylesheet" />
 
